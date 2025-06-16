98a3f13fda89de262c68257e18fd78971acb157c
200103366 <91107509+200103366@users.noreply.github.com>
2025-06-17 02:16:35 +0500

    asgi.py

--- broma_config/asgi.py
+++ broma_config/asgi.py
@@ -1,20 +1,22 @@
 import os
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "broma_config.settings.local")
 
+# HTTP-приложение Django
 django_asgi_app = get_asgi_application()
 
-import conversations.routing
+# Импорты для WebSocket
 from channels.routing import ProtocolTypeRouter, URLRouter
+import conversations.routing
 from conversations.middlewares import ConversationUserSessionMiddlewareStack
 
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": ConversationUserSessionMiddlewareStack(
-            URLRouter(conversations.routing.websocket_urlpatterns),
-        ),
-    }
-)
+# Объединённый ASGI application
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": ConversationUserSessionMiddlewareStack(
+        URLRouter(
+            conversations.routing.websocket_urlpatterns
+        )
+    ),
+})
