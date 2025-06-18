378a69008dce8a556f49580400d3f11bef89e9d3
200103366 <91107509+200103366@users.noreply.github.com>
2025-06-19 02:47:32 +0500

    production.py

--- broma_config/settings/production.py
+++ broma_config/settings/production.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 
 # === Middleware ===
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -37,6 +38,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    
 ]
 
 # === URL и ASGI ===
@@ -81,6 +83,7 @@ CHANNEL_LAYERS = {
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # === Часовой пояс и язык ===
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
