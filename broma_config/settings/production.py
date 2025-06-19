2700357cf0f64a0bcfc9c91cf6689a9febd7eb88
200103366 <91107509+200103366@users.noreply.github.com>
2025-06-19 15:13:07 +0500

    production.py

--- broma_config/settings/production.py
+++ broma_config/settings/production.py
@@ -13,7 +13,7 @@ environ.Env.read_env(env_file=BASE_DIR / ".env")  # работает и лока
 # === Основные настройки ===
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=False)
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[".onrender.com", "localhost", "127.0.0.1"])
+ALLOWED_HOSTS = ['videochat2-0sxs.onrender.com', '127.0.0.1', 'localhost']
 
 # === Установленные приложения ===
 INSTALLED_APPS = [
